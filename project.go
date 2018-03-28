fe528fb8db53e71a24198333d52814e34d4f039a
NUZHAFIQ NOOR AZMAN - Student <G00324934@gmit.ie>
2018-03-28 15:25:07 +0100

    first commit

---
+++ project.go
@@ -0,0 +1,19 @@
+package main 
+
+import(
+	"fmt"
+)
+
+func intopost(infix)string{
+	postfix := ""
+
+	return postfix
+}
+
+func main(){
+	//Answer: ab.c*
+	fmt.Println("Infix: ","a.b.c*");
+	fmt.Println("Postifx: ",intopost("a.b.c*"))
+
+	
+}
