6bbb1b151435f4cce939063211b204d5d8183815
aefiq96 <g00324934@gmit.ie>
2018-04-07 00:33:13 +0100

    nfa case update

--- nfa.go
+++ nfa.go
@@ -30,7 +30,19 @@ func poregtonfa(pofix string) *nfa {
 
             nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
 
-        case '|':            
+        case '|':   
+            frag2 := nfastack[len(nfastack)-1]
+            nfastack = nfastack[:len(nfastack)-1]
+            frag1 := nfastack[len(nfastack)-1]
+            nfastack = nfastack[:len(nfastack)-1]
+
+            accept := state{}
+            initial := state{edge1: frag1.initial, edge2: frag2.initial}
+            frag1.accept.edge1 = &accept
+            frag2.accept.edge1 = &accept
+
+
+            nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})         
         case '*':            
         default:            
         }
