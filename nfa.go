7bba07841dd7e39f5d55658608c7110f660d0738
aefiq96 <g00324934@gmit.ie>
2018-04-07 00:31:44 +0100

    nfa case update

--- nfa.go
+++ nfa.go
@@ -18,6 +18,24 @@ type nfa struct{
 func poregtonfa(pofix string) *nfa { 
     nfastack := []*nfa{}   
 
+    for _, r := range pofix {
+        switch r {
+        case '.':          
+            frag2 := nfastack[len(nfastack)-1]
+            nfastack = nfastack[:len(nfastack)-1]
+            frag1 := nfastack[len(nfastack)-1]
+            nfastack = nfastack[:len(nfastack)-1]           
+
+            frag1.accept.edge1 = frag2.initial
+
+            nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
+
+        case '|':            
+        case '*':            
+        default:            
+        }
+        }
+    return nfastack[0]
 }
 
 func main(){
